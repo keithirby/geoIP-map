b728e1328dfb92b0685433978437676ef4e10fa4
kirby <kirby@twosix.net>
2025-08-30 19:20:53 -0400

    Updating sender to use a single database with two tables

--- proj/remote/src/db.py
+++ proj/remote/src/db.py
@@ -4,7 +4,7 @@
 and provide factories to start sessions for our sqllite engines
 """
 # Per the user agreement I signed I cannot upload the databases I used to the internet
-from config import COUNTRY_CVS, BLOCKS_CVS, DB_COUNTRY_PATH, DB_BLOCK_PATH
+from config import COUNTRY_CVS, BLOCKS_CVS, GEO_IP_DB_PATH 
 
 # Other libraries needed
 import pandas as pd
@@ -14,14 +14,13 @@ from sqlalchemy.orm import sessionmaker
 # Database setup
 # -----------------------
 
-# Two global engines for our main databases  
-ENGINE_COUNTRIES = create_engine(f"sqlite:///{DB_COUNTRY_PATH}", echo=False)
-ENGINE_BLOCKS  = create_engine(f"sqlite:///{DB_BLOCK_PATH}", echo=False)
+# One  global engines for our main databases  
+GEOIP_ENGINE = create_engine(f"sqlite:///{GEO_IP_DB_PATH}", echo=False)
 
-# Two global session factories
+
+# One global session factories
 """ Used with helper functions to create session query instances"""
-SESS_COUNTRY_FACTORY = sessionmaker(bind=ENGINE_COUNTRIES)
-SESS_BLOCK_FACTORY = sessionmaker(bind=ENGINE_BLOCKS)
+SESS_GEOIP_FACTORY = sessionmaker(bind=GEOIP_ENGINE)
 
 """!
 @brief Load a passed csv_file into a engine
@@ -30,28 +29,24 @@ def load_csv_to_sqlite(csv_file, table_name, engine):
     """Load CSV into SQLite using pandas"""
     df = pd.read_csv(csv_file)
     df.to_sql(table_name, engine, if_exists="replace", index=False)
+    print(f"Loaded {len(df)} rows into table '{table_name}'")
+    
+    
 
 """!
 @brief Initialize our two databases with our two csv files
 """
 def initalize_engines(): 
-    print("Loading engines with csv data")
     # 2. Load CSVs into DBs
-    load_csv_to_sqlite(COUNTRY_CVS, "countries", ENGINE_COUNTRIES)
-    load_csv_to_sqlite(BLOCKS_CVS, "blocks", ENGINE_BLOCKS)
+    print("Loading GEOIP_ENGINE with CSV data")
+    load_csv_to_sqlite(COUNTRY_CVS, "countries", GEOIP_ENGINE)
+    load_csv_to_sqlite(BLOCKS_CVS, "blocks", GEOIP_ENGINE)
+    print("Initialization complete.")
 
 """!
-@brief helper function to return a ENGINE_COUNTRIES session to 
+@brief helper function to return a  GEOIP_ENGINE session to 
 query the engine
 """
-def get_country_session():
-    """Returns a new session bound to the countries database."""
-    return SESS_COUNTRY_FACTORY()
-
-"""!
-@brief helper function to return a ENGINE_BLOCKS session to 
-query the engine
-"""
-def get_block_session():
-    """Returns a new session bound to the blocks database."""
-    return SESS_BLOCK_FACTORY()
+def get_geoip_session():
+    """Returns a new session bound to the combined GEOIP_ENGINE database."""
+    return SESS_GEOIP_FACTORY()
--- proj/remote/src/main.py
+++ proj/remote/src/main.py
@@ -14,8 +14,8 @@ import time # for sleep
 from sqlalchemy import text
 
 # Local libraries we need
-from db import initalize_engines, get_country_session, get_block_session
-from config import countries_list
+from db import initalize_engines, get_geoip_session
+from config import countries_list_selected
 from scapy_send import send_packet
 
 
@@ -35,51 +35,46 @@ signal.signal(signal.SIGINT, signal_handler)
 #  setup and start
 # -----------------------
 def main():
-    # 1. load the csv files into our two engines 
-    # We dont need to start the engines because they initialize on boot 
-    # in db.py 
-    initalize_engines()
+    # 1. Wait on the user to start the event loop and let them choose  
+    # to load the load and cvs files into it if we havent already 
+    print("Initialized. Hit enter to start... \n Type `yes` to initalize databases")
+    user_ready = False
+    user_input = input().strip().lower()
+    if user_input == "yes":
+        user_ready = True
+        initalize_engines()
+    
 
-    # 2. Create sessions to access the database
-    session_countries = get_country_session()
-    session_blocks = get_block_session()
+    # 2. Create sessions to access the geoIP database database
+    geoip_session = get_geoip_session()
     print("Sessions created")
 
-    # 3. Wait on the user to start the event loop
-    print("Databases initialized. Type 'yes' to continue...")
-    user_ready = False
-    while not user_ready:
-        user_input = input().strip().lower()
-        if user_input == "yes":
-            user_ready = True 
+    # Make a list of all possible countries from countries table    
+    stmt = text("SELECT country_name, geoname_id FROM countries")
+    country_records = geoip_session.execute(stmt).fetchall()
+    country_list = [(row[0], row[1]) for row in country_records]
         
     # 4. Start the loop and print countries and their major IP block address 
     # pulled from the database
     while not INTERRUPTED:
-        # OPTION 1: totally random country + IP ADDR
+        # OPTION 1: totally random country + IP address
         # Get country_name : geoname_id pairs
-        stmt = text("SELECT country_name, geoname_id FROM countries")
-        country_rows = session_countries.execute(stmt).fetchall()
-        # Picks random country from the database
-        rnd_country_list = [(row[0], row[1]) for row in country_rows]
-        country_name, geoname_id = random.choice(rnd_country_list)
+        country_name, geoname_id = random.choice(country_list)
         #-------------------------------------------------------
-        # OPTION 2: Random ish country from pre-defined list
-        #country_name, geoname_id = random.choice(countries_list)
-        # Helper prints for debugging
+        # OPTION 2: Random ish country from pre-defined list in config.py
+        #country_name, geoname_id = random.choice(countries_list_selected)
+        #--------------------------------------------------------
         # geoname_id print for debugging
         #print(f"Random country: {country_name} (geoname_id={geoname_id})")
-        # normal print for not debugging
-        #print(f"Random country: {country_name}")
 
-        # Query blocks table using text()
+        # Query blocks table for all Major IP blocks for a country
         block_stmt = text("SELECT network FROM blocks WHERE geoname_id = :gid")
-        block_rows = session_blocks.execute(block_stmt, {"gid": geoname_id}).fetchall()
+        block_rows = geoip_session.execute(block_stmt, {"gid": geoname_id}).fetchall()
 
         if not block_rows:
             print("No matching block entries found.")
         else:
-            # Pick a random block entry
+            # Pick a random Major IP Block for a country
             random_network = random.choice(block_rows)[0]
             #print(f"Sending network for {country_name} : {random_network}")
             #Send the packet over the network
